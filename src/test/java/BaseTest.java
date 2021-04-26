20210892a1482dd603c1cfaed7f8c93dc9ebbb89
katyazy <ek.ziateva@gmail.com>
2021-04-26 15:18:22 +0300

    added tests to check the Issue name in the repository

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,11 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest {
+
+    @BeforeAll
+    public static void setup() {
+        Configuration.startMaximized = true;
+        Configuration.baseUrl = "https://github.com";
+    }
+}
